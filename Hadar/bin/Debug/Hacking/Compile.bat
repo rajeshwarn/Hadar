b4cebd571eb2881bb3b3d2dc6aa99fa6d5739355
Alexandro Luongo <W00dL3cs@users.noreply.github.com>
2016-12-02 21:42:49 +0100

    Added missing files.

---
+++ Hadar/bin/Debug/Hacking/Compile.bat
@@ -0,0 +1,6 @@
+RABCDasm\rabcasm %1-0/%1-0.main.asasm
+RABCDasm\abcreplace %1.swf 0 %1-0/%1-0.main.abc
+RABCDasm\rabcasm %1-2/%1-2.main.asasm
+RABCDasm\abcreplace %1.swf 2 %1-2/%1-2.main.abc
+RABCDasm\rabcasm %1-1/%1-1.main.asasm
+RABCDasm\abcreplace %1.swf 1 %1-1/%1-1.main.abc
---
+++ Hadar/bin/Debug/Hacking/Decompile.bat
@@ -0,0 +1,3 @@
+RABCDasm\abcexport %1.swf
+RABCDasm\rabcdasm %1-0.abc
+RABCDasm\rabcdasm %1-1.abc
